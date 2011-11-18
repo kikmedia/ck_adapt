b8d1211eed1abf09b0d02c25002974e329f06bd6
Carolina M. Koehn <office@kikmedia.de>
2011-11-18 08:43:05 +0100

    Adds a function to extend #wrapper with needed .container class

---
+++ plugins/adapt/wrapextender.js
@@ -0,0 +1,7 @@
+/*
+Wrapextender.js licensed under GPL and MIT.
+(c) 2011 kikmedia webdevelopment
+
+Simply adds a class .container_12 to the #wrapper element so that there will be no need to edit fe_page manually
+*/
+$('wrapper').addClass('container_12');
